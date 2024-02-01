5d1ee369839daee25acb35445611193ead84f852
vijay5599 <59635616+vijay5599@users.noreply.github.com>
2024-02-01 07:12:30 +0530

    Time: 95 ms (56.09%), Space: 54.2 MB (7.11%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    //sort the array in ascending order
+  nums.sort((a, b) => a - b);
+
+  // Calculate the product of the three largest elements
+  const n = nums.length;
+  const product1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+
+  // Calculate the product of the two smallest elements and the largest element
+  const product2 = nums[0] * nums[1] * nums[n - 1];
+
+  // Return the maximum of the two products
+  return Math.max(product1,product2);
+};
